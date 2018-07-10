a7fb94e6fdaa35934280245c64fcb7ec6009387e
Jorge Gonzalez <jrgqhd@gmail.com>
2018-07-10 19:18:16 -0400

    Refactor

--- src/cli.js
+++ src/cli.js
@@ -1,24 +1,11 @@
-import path from 'path';
 import updateNotifier from 'update-notifier';
 import meow from 'meow';
 import chalk from 'chalk';
-import eslint from 'eslint';
 
 import pkg from '../package';
-import { korra } from './utils';
 import Engine from './engine';
 
 updateNotifier({ pkg }).notify();
-
-const defaultOptions = {
-  version: pkg.version,
-  homepage: pkg.homepage,
-  bugs: pkg.bugs,
-  cmd: 'korra',
-  eslintConfig: {
-    configFile: path.join(__dirname, 'eslintrc.json')
-  }
-};
 
 let { input, flags } = meow(
   chalk`
@@ -38,20 +25,19 @@ Usage:
 );
 
 export default () => {
-  console.log(korra, pkg.version);
+  console.log(chalk.bold.cyan('Korra'), pkg.version);
 
-  let linter = new Engine(defaultOptions);
-  let formatter = linter.CLIEngine.getFormatter('pretty');
+  let korra = new Engine();
+  let formatter = korra.CLIEngine.getFormatter('pretty');
 
-  if (input === undefined || input.length === 0) {
+  if (input.length === 0) {
     input = ['**/*.js', '**/*.jsx'];
   }
 
-  let report = linter.lint(input, flags.fix);
-  // console.log(report)
+  let report = korra.lint(input, flags.fix);
 
   if (flags.fix) {
-    linter.outputFixes(report);
+    korra.outputFixes(report);
   }
 
   console.log(formatter(report.results));
--- src/engine.js
+++ src/engine.js
@@ -4,32 +4,35 @@ import eslint from 'eslint';
 import pkgConf from 'pkg-conf';
 import globby from 'globby';
 
+import pkg from '../package';
+
 export default class Engine {
-  constructor(options) {
-    let m = options.version && options.version.match(/^(\d+)\./);
-    let majorVersion = (m && m[1]) || '0';
+  constructor() {
+    let defaults = {
+      version: pkg.version,
+      eslintConfig: {
+        configFile: path.join(__dirname, 'eslintrc.json')
+      }
+    };
 
-    let cache = path.join(
+    let cacheLocation = path.join(
       os.homedir() || os.tmpdir(),
-      `.${this.cmd}-v${majorVersion}-cache/`
+      `.korra-v${defaults.version}-cache/`
     );
 
-    this.eslintConfig = Object.assign(
-      {
-        cache: true,
-        cacheLocation,
-        envs: [],
-        fix: false,
-        globals: [],
-        ignore: false,
-        plugins: [],
-        useEslintrc: false,
-        rules: {}
-      },
-      options.eslintConfig
-    );
+    this.eslintConfig = {
+      cache: true,
+      cacheLocation,
+      envs: [],
+      fix: false,
+      globals: [],
+      ignore: false,
+      plugins: [],
+      useEslintrc: false,
+      rules: {},
+      ...defaults.eslintConfig
+    };
 
-    this.cmd = options.cmd;
     this.options = this.parseoptions();
     this.CLIEngine = new eslint.CLIEngine(this.options.eslintConfig);
     this.outputFixes = eslint.CLIEngine.outputFixes;
@@ -44,29 +47,27 @@ export default class Engine {
         '**/bundle.js',
         'coverage/**',
         'node_modules/**',
-        'vendor/**'
+        'vendor/**',
+        'dist/**'
       ]
     };
 
-    let packageoptions = pkgConf.sync(this.cmd, {
-      cwd: options.cwd
-    });
-
+    let packageOptions = pkgConf.sync('korra');
     let configurableKeys = ['rules', 'globals', 'plugins', 'envs', 'parser'];
 
     configurableKeys.forEach(key => {
-      if (key in packageoptions) {
+      if (key in packageOptions) {
         if (key === 'rules') {
-          options.eslintConfig.rules = Object.assign(
-            packageoptions.rules,
-            this.eslintConfig.rules
-          );
+          options.eslintConfig.rules = {
+            ...packageOptions.rules,
+            ...this.eslintConfig.rules
+          };
         } else if (key === 'parser') {
-          options.eslintConfig.parser = packageoptions.parser;
+          options.eslintConfig.parser = packageOptions.parser;
         } else {
           // The other options map to arrays
           options.eslintConfig[key] = this.eslintConfig[key].concat(
-            packageoptions[key]
+            packageOptions[key]
           );
         }
       }
