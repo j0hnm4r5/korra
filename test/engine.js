b91d9fd1b034a87f05dcb4c1aa98b4a4f6b484b3
Jorge Gonzalez <jrgqhd@gmail.com>
2018-07-13 22:30:54 -0400

    Add test

--- test/engine.js
+++ test/engine.js
@@ -1,19 +1,34 @@
 import path from 'path';
 
-import Engine from '../src/engine';
+import korra from '../src/engine';
 import envs from './fixtures/envs/package';
+import parser from './fixtures/parser/package.json';
 
-describe('Korra Engine', () => {
+describe('Korra korra', () => {
   it('should pass a baseline test', () => {
     expect(true).toBe(true);
   });
 
-  it('should read a valid package.json file', async () => {
-    let engine = new Engine(path.join(__dirname, './fixtures/envs'));
-    engine.lint('./engine.js');
+  it('should set envs from package.json', () => {
+    let { options } = korra.lint(
+      './korra.js',
+      false,
+      path.join(__dirname, './fixtures/envs')
+    );
 
-    let [mongo, jquery] = envs.korra.envs;
-    expect(engine.options.envs).toContain(mongo);
-    expect(engine.options.envs).toContain(jquery);
+    const [mongo, jquery] = envs.korra.envs;
+    expect(options.envs).toContain(mongo);
+    expect(options.envs).toContain(jquery);
+  });
+
+  it('should set parser from package.json', () => {
+    let { options } = korra.lint(
+      './korra.js',
+      false,
+      path.join(__dirname, './fixtures/parser')
+    );
+
+    const tsParser = parser.korra.parser;
+    expect(options.parser).toBe(tsParser);
   });
 });
